4ccf82f5840ba4a6c4d53ee75c126b555b952f40
Ryan Schmukler <rschmukler@gmail.com>
2011-12-14 16:36:26 -0600

    Added mfs.h defintion file and created mfs.c

---
+++ mfs.c
@@ -0,0 +1,29 @@
+#include "mfs.h"
+
+int MFS_Init(char *hostname, int port) {
+	
+}
+
+int MFS_Lookup(int pinum, char *name){
+	
+}
+
+int MFS_Stat(int inum, MFS_Stat_t *m) {
+	
+}
+
+int MFS_Write(int inum, char *buffer, int block){
+	
+}
+
+int MFS_Read(int inum, char *buffer, int block){
+	
+}
+
+int MFS_Creat(int pinum, int type, char *name){
+	
+}
+
+int MFS_Unlink(int pinum, char *name){
+	
+}
